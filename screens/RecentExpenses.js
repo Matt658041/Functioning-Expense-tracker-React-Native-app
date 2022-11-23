09dd19688595c19252d23c071b1e26ebaddb6644
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-11-23 08:50:48 -0600

    missed an array in expenses context provider

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,18 +1,17 @@
 import { useContext, useEffect,} from "react";
-
 import ExpensesOutput from "../components/Expenses Output/ExpensesOutput";
-import { ExpensesContext } from "../store/expenses-context";
+import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
-  const expensesCtx = useContext(ExpensesContext);
+  const expensesCtx = useContext(ExpensesContext)
 
 
   useEffect(() => {
     async function getExpenses() {
       const expenses = await fetchExpenses();
-      expensesCtx.setExpenses(expenses)
+      expensesCtx.setExpenses(expenses);
     }
     getExpenses();
   }, []);
@@ -22,7 +21,7 @@ function RecentExpenses() {
     const date7DaysAgo = getDateMinusDays(today, 7);
 
     return expense.date >= date7DaysAgo && expense.date <= today;
-  });
+  })
 
   return (
     <ExpensesOutput
--- store/expenses-context.js
+++ store/expenses-context.js
@@ -34,7 +34,7 @@ function expensesReducer(state, action) {
 }
 
 function ExpensesContextProvider({ children }) {
-  const [expensesState, dispatch] = useReducer(expensesReducer);
+  const [expensesState, dispatch] = useReducer(expensesReducer, []);
 
   function addExpense(expenseData) {
     dispatch({ type: "ADD", payload: expenseData });
